3cd3c7e4f816194813e3848d793ec7f19a20904e
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-12 13:04:15 +0600

    header issue fixed

--- components/header/header.js
+++ components/header/header.js
@@ -2,12 +2,13 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import { MdClose, MdMenu } from 'react-icons/md';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Header = () => {
     const [openMenu, setOpenMenu] = useState(false)
     return (
         <div className='bg-primary px-5 md:px-10 lg:px-20 sticky top-0 lg:pt-2'>
-            <div className='flex justify-between items-center'>
+            <div className='flex justify-between items-center relative'>
                 <div onClick={() => setOpenMenu(!openMenu)} className='lg:hidden'>
                     <MdMenu className='text-base-100 ' size={30} />
                 </div>
@@ -18,26 +19,38 @@ const Header = () => {
                     <li className='px-4 py-2 lg:py-0'><a className='text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#blogs">Blogs</a></li>
                     <li className='px-4 py-2 lg:py-0'><a className='text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#contact">Contact</a></li>
                 </ul>
+                <div className={`flex justify-between items-start z-10 py-5 bg-secondary w-64 absolute transition-all duration-500 ease-in-out ${openMenu ? 'top-[0px]' : 'top-[-300px]'}`}>
+                    {
+                        openMenu &&
+                        <ul >
+                            <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><Link className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="/">Home</Link></li>
+                            <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#about">About</a></li>
+                            <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#project">Projects</a></li>
+                            <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#blogs">Blogs</a></li>
+                            <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#contact">Contact</a></li>
+                        </ul>
+                    }
+                    <AiOutlineCloseCircle onClick={() => setOpenMenu(!openMenu)} className='cursor-pointer mr-3 absolute left-52' color='#fff' size={25} />
+                </div>
 
-                {
-                    openMenu &&
-                    <ul className={`z-10 py-5 bg-secondary w-64 absolute transition duration-500 ease-in-out ${openMenu ? 'top-[0px]' : 'top-[-300px]'}`}>
-                        <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><Link className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="/">Home</Link></li>
-                        <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#about">About</a></li>
-                        <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#project">Projects</a></li>
-                        <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#blogs">Blogs</a></li>
-                        <li onClick={() => setOpenMenu(!openMenu)} className='px-4 py-2 lg:py-0'><a className='text-xl lg:text-2xl text-base-100 lg:text-neutral hover:text-base-100 transition-all ease-in-out duration-300' href="#contact">Contact</a></li>
-                    </ul>
-                }
 
-                <div className='pt-5 lg:pt-0 lg:hidden'>
+                <div className='pt-2 lg:pt-0 lg:hidden'>
                     <a href='#contact'>
                         <Button sx={{
                             borderRadius: '45px',
-                            padding: '15px 40px',
+                            padding: {
+                                sm:'8px 20px',
+                                md:'16px 40px'
+                            },
                             border: '1px solid #65666D',
                             color: '#65666D',
                             fontSize: '16px',
+                            fontSize: {
+                                xs:'11px',
+                                sm:'12px',
+                                lg:'16px',
+                                xl: '16px'
+                            },
                             fontWeight: '700',
                             '&:hover': {
                                 color: '#fff', border: '1px solid #fff', background: 'transparent'
